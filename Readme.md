fd3fee28c1070692da40bf2cb36b70117424c35d
dheeraj <itsvicke6531@gmail.com>
2024-07-16 11:14:05 +0530

    day 1

---
+++ Readme.md
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+<!-- echo "# Crack_Your_Placment" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Vicke413/Crack_Your_Placment.git
+git push -u origin main -->
---
+++ [1] Array/01_Find_the_Duplicate_number.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+int n=nums.size();
+set<int>non_duplicates;
+
+        for(int i=0;i<n;i++)
+        {
+            int initial_size=non_duplicates.size();
+            non_duplicates.insert(nums[i]);
+            int last_size=non_duplicates.size();
+
+            if(last_size==initial_size)
+            return nums[i];
+        }
+
+        return -1;
+
+        TC-O(N)
+        SC-O(N)
+
+    }
+};
+
+*/
+
